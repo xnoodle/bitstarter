d0a24718b3d06cdbfd9a65366a8d2c3811fa531d
Joe Lee <xnoodle@gmail.com>
2013-07-09 17:01:42 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function(err,data) {
+    if (err) throw err;
+    var buf = new Buffer(data);
+    response.send(buf.toString());
+   });
 });
 
 var port = process.env.PORT || 5000;
